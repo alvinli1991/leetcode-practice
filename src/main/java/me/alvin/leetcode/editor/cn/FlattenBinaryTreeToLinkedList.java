69cf0b0c35cdfb9e30c2d90d63db40102d0fcf3a
Alvin Li <lxlx1991@gmail.com>
2021-03-03 22:14:54 +0800

    114

---
+++ src/main/java/me/alvin/leetcode/editor/cn/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,136 @@
+//给你二叉树的根结点 root ，请你将它展开为一个单链表： 
+//
+// 
+// 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。 
+// 展开后的单链表应该与二叉树 先序遍历 顺序相同。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,2,5,3,4,null,6]
+//输出：[1,null,2,null,3,null,4,null,5,null,6]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [0]
+//输出：[0]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中结点数在范围 [0, 2000] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶：你可以使用原地算法（O(1) 额外空间）展开这棵树吗？ 
+// Related Topics 树 深度优先搜索 
+// 👍 737 👎 0
+
+
+package me.alvin.leetcode.editor.cn;
+
+public class FlattenBinaryTreeToLinkedList {
+    public static void main(String[] args) {
+        Solution solution = new FlattenBinaryTreeToLinkedList().new Solution();
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        node1.left = node2;
+        node1.right = node5;
+        node2.left = node3;
+        node2.right = node4;
+        node5.right = node6;
+        solution.flatten(node1);
+        System.out.println(node1);
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public void flatten(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+            flatten(root.left);
+            flatten(root.right);
+
+            //将根节点的右子树放到左子树最后一个节点的右节点
+            TreeNode lastNode = findLastRightNode(root.left);
+            if (lastNode != null) {
+                lastNode.right = root.right;
+                //将根的右子树设置为左子树,根的左子树赋值为null
+                root.right = root.left;
+                root.left = null;
+            }
+
+        }
+
+        public TreeNode findLastRightNode(TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+            TreeNode temp = root;
+            while (temp.right != null) {
+                temp = temp.right;
+            }
+            return temp;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+    //Definition for a binary tree node.
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+}
