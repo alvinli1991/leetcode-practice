e86a451bc6cfe81c0cf6584adb12422071ed3807
Alvin Li <lxlx1991@gmail.com>
2021-05-05 20:22:41 +0800

    474 增加二维数组

--- src/main/java/me/alvin/leetcode/editor/cn/OnesAndZeroes.java
+++ src/main/java/me/alvin/leetcode/editor/cn/OnesAndZeroes.java
@@ -93,6 +93,27 @@ public class OnesAndZeroes {
             return result;
         }
 
+        public int findMaxForm2Dim(String[] strs, int m, int n) {
+            int[][] dp = new int[m + 1][n + 1];
+            for (String s : strs) {
+                int[] count = countZeroesOnes(s);
+                for (int zeroes = m; zeroes >= count[0]; zeroes--) {
+                    for (int ones = n; ones >= count[1]; ones--) {
+                        dp[zeroes][ones] = Math.max(1 + dp[zeroes - count[0]][ones - count[1]], dp[zeroes][ones]);
+                    }
+                }
+            }
+            return dp[m][n];
+        }
+
+        public int[] countZeroesOnes(String s) {
+            int[] c = new int[2];
+            for (int i = 0; i < s.length(); i++) {
+                c[s.charAt(i) - '0']++;
+            }
+            return c;
+        }
+
     }
 
     class ItemCount {
