20e8896145331fd4adee27ae4f8a42af44bb2e02
Alvin Li <lxlx1991@gmail.com>
2021-05-10 11:22:32 +0800

    添加通用辅助类

---
+++ src/main/java/support/TreeTools.java
@@ -0,0 +1,24 @@
+package support;
+
+
+import me.alvin.leetcode.editor.cn.TreeNode;
+
+/**
+ * @author Alvin Li
+ * @since 2021/5/10 11:10
+ */
+public class TreeTools {
+    public static TreeNode buildTree(Integer[] nodes, TreeNode root, int i) {
+        if (i < nodes.length) {
+            if(nodes[i] != null){
+                TreeNode temp = new TreeNode(nodes[i]);
+                root = temp;
+
+                root.left = buildTree(nodes, root.left, 2 * i + 1);
+
+                root.right = buildTree(nodes, root.right, 2 * (i + 1));
+            }
+        }
+        return root;
+    }
+}
