cc10f7bc9a6e616e9780a006b82c5b8239777898
Alvin Li <lxlx1991@gmail.com>
2021-05-19 13:04:16 +0800

    翻转链表

---
+++ src/main/java/me/alvin/leetcode/editor/cn/ReverseLinkedList.java
@@ -0,0 +1,106 @@
+//给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+// 
+// 
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5]
+//输出：[5,4,3,2,1]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1,2]
+//输出：[2,1]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目范围是 [0, 5000] 
+// -5000 <= Node.val <= 5000 
+// 
+//
+// 
+//
+// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？ 
+// 
+// 
+// Related Topics 链表 
+// 👍 1745 👎 0
+
+
+package me.alvin.leetcode.editor.cn;
+
+import support.ListNode;
+import support.ListTools;
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        Solution solution = new ReverseLinkedList().new Solution();
+        ListNode head = ListTools.buildLinkedList(new int[]{1,2,3,4,5});
+
+
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        /**
+         * 递归算法
+         *
+         * @param head
+         * @return
+         */
+        public ListNode reverseList2(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode last = reverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return last;
+        }
+
+        /**
+         * 非递归算法
+         *
+         * @param head
+         * @return
+         */
+        public ListNode reverseList(ListNode head) {
+            ListNode pre = null;
+            ListNode current = head;
+            while (current != null) {
+                ListNode next = current.next;
+                current.next = pre;
+                pre = current;
+                current = next;
+            }
+            return pre;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
