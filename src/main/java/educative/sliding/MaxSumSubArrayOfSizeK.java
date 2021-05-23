e28211062f6ff7a7562d7de21c7d75188ed1e4e1
Alvin Li <lxlx1991@gmail.com>
2021-05-23 21:22:08 +0800

    增加问题描述

--- src/main/java/educative/sliding/MaxSumSubArrayOfSizeK.java
+++ src/main/java/educative/sliding/MaxSumSubArrayOfSizeK.java
@@ -1,6 +1,8 @@
 package educative.sliding;
 
 /**
+ * Given an array of positive numbers and a positive number ‘k,’ find the maximum sum of any contiguous subarray of size ‘k’.
+ *
  * @author Alvin Li
  * @since 2021/5/23 16:15
  */
--- src/main/java/educative/sliding/MinSizeSubArraySum.java
+++ src/main/java/educative/sliding/MinSizeSubArraySum.java
@@ -1,6 +1,7 @@
 package educative.sliding;
 
 /**
+ * Given an array of positive numbers and a positive number ‘S,’ find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0 if no such subarray exists.
  * @author Alvin Li
  * @since 2021/5/23 16:55
  */
