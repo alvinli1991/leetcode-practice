b03697bfd43ae2a198499305a7a5eaf9b23191f7
Alvin Li <lxlx1991@gmail.com>
2021-05-23 17:09:49 +0800

    使用最大值来精简代码

--- src/main/java/educative/sliding/MinSizeSubArraySum.java
+++ src/main/java/educative/sliding/MinSizeSubArraySum.java
@@ -6,9 +6,9 @@ package educative.sliding;
  */
 public class MinSizeSubArraySum {
     public static void main(String[] args) {
-        System.out.println(findMinSubArray(7, new int[]{2, 1, 5, 2, 3, 2}));
-        System.out.println(findMinSubArray(7, new int[]{2, 1, 5, 2, 8}));
-        System.out.println(findMinSubArray(8, new int[]{3, 4, 1, 1, 6}));
+        System.out.println(findMinSubArray2(7, new int[]{2, 1, 5, 2, 3, 2}));
+        System.out.println(findMinSubArray2(7, new int[]{2, 1, 5, 2, 8}));
+        System.out.println(findMinSubArray2(8, new int[]{3, 4, 1, 1, 6}));
     }
 
     public static int findMinSubArray(int s, int[] arr) {
@@ -34,5 +34,32 @@ public class MinSizeSubArraySum {
             }
         }
         return minWindowSize;
+    }
+
+    /**
+     * 使用最大值设置窗口最大边界
+     * @param s
+     * @param arr
+     * @return
+     */
+    public static int findMinSubArray2(int s, int[] arr) {
+        int left = 0;
+        int right = 0;
+        int minWindowSize = Integer.MAX_VALUE;
+        int windowSize = 0;
+        int windowSum = 0;
+        while (right < arr.length) {
+            windowSum += arr[right];
+            right++;
+            windowSize++;
+
+            while (windowSum >= s) {
+                minWindowSize = Math.min(minWindowSize, windowSize);
+                windowSum -= arr[left];
+                left++;
+                windowSize--;
+            }
+        }
+        return minWindowSize == Integer.MAX_VALUE ? 0 : minWindowSize;
     }
 }
